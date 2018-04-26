430585d6081a39b336cda0b2fa81f636eca9ab1f
Ryan Van Ess <fallen.rve@gmail.com>
2018-04-25 20:59:18 -0500

    adds extract css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    devtool: 'source-map',
-    output: {
-        filename: 'app.js',
-        path: path.join(__dirname, "/dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
+module.exports = (env) => {
+    const devMode = env.env !== 'production';
+    return {
+        entry: './src/app.js',
+        devtool: 'source-map',
+        output: {
+            filename: 'app.js',
+            path: path.join(__dirname, "/dist")
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                    ],
+                }
 
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + '/src/index.html'
+            }),
+            new MiniCssExtractPlugin('app.css')
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html'
-        }),
-    ]
+    };
 };
