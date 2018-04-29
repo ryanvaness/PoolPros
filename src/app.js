e8264fa6ddea5e55f3156ee52cba143616f5e2f9
Ryan Van Ess <fallen.rve@gmail.com>
2018-04-29 13:36:59 -0500

    adds functionality for mobile menu

--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
 const styles = require('./app.scss');
-console.log('test webpack');
-const arr = ['a','s','d','f'];
-console.log(...arr);
+
+document.addEventListener('click', function delegateClick(event) {
+    const clickedElement = event.target;
+    if (clickedElement.matches('.mobile-menu') || clickedElement.matches('.mobile-menu > div')) {
+        openMenu();
+    } else if (clickedElement.matches('.close-menu')) {
+        closeMenu();
+    }
+
+    if (clickedElement.matches('.overlay')) {
+        closeCurrent();
+    }
+});
+
+const openMenu = () => {
+    toggleOverlay();
+    document.querySelector('nav.primary').classList.add('show');
+}
+const closeMenu = () => {
+    toggleOverlay();
+    document.querySelector('nav.primary').classList.remove('show');
+}
+
+const toggleOverlay = () => {
+    document.body.classList[document.body.classList.contains('open') ? 'remove' : 'add']('open');
+}
+
+const closeCurrent = () => {
+    if (document.querySelector('nav.primary').classList.contains('show')) {
+        closeMenu();
+    } else if (document.querySelector('.modal').classList.contains('show')) {
+
+    }
+}
